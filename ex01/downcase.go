97cf67aa7d088ab6ee94c0077b8ad0b65c69c6a1
chumaknadya <chumaknadya19@gmail.com>
2018-07-12 16:44:44 +0300

    Done

--- ex01/downcase.go
+++ ex01/downcase.go
@@ -1,13 +1,13 @@
 package downcase
 
 func Downcase(initStr string) (string, error) {
-  arr := make([]byte, len(initStr))
-  for i := range arr {
-    letter := initStr[i]
-    if letter >= 'A' && letter <= 'Z' {
-      letter += 'a' - 'A'
-    }
-    arr[i] = letter
- }
- return string(arr), nil
+	arr := make([]byte, len(initStr))
+	for i := range arr {
+		letter := initStr[i]
+		if letter >= 'A' && letter <= 'Z' {
+			letter += 'a' - 'A'
+		}
+		arr[i] = letter
+	}
+	return string(arr), nil
 }
